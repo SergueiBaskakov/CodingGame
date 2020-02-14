a8deac5c9ef4b21c861b62e8385a32cb5d7594da
SergueiBaskakov <46057775+SergueiBaskakov@users.noreply.github.com>
2020-02-14 11:57:59 -0500

    Funciona con todos los test case

--- PaperLabyrinth.cpp
+++ PaperLabyrinth.cpp
@@ -49,48 +49,81 @@ int main()
             else if(l[j] == 'd'){
                 lab[i][j].valor = 13;
             }
+            else if(l[j] == 'e'){
+                lab[i][j].valor = 14;
+            }
+            else if(l[j] == 'f'){
+                lab[i][j].valor = 15;
+            }
             else{
                 lab[i][j].valor = int(l[j])-48;
             }
         }
     }
-    
-    function<string()> respuesta = [&lab]() -> string {
+    //function<void()> respuesta = [&lab,&xs,&ys,&xr,&yr]() -> void {
         int distancia = -1;
-        function<int(int)> calc_dist = [&lab](int posX, int posY, int dist) -> int {
-            int val = lab[posX][posY];
+        //function<int(int,int,int,int,int)> calc_dist; 
+        auto calc_dist = [&distancia](int posX, int posY, int dist, int finX, int finY, auto& calc_dist, auto* lab) -> int {
+            int val = (*lab)[posX][posY].valor;
             bool fin = 1;
-            lab[posX][posY].acceso=0;
-            if(val<8 && lab[posX][posY+1].acceso==1){
-                calc_dist(posX, posY+1,dist+1);
-                fin = 0;
-            }
-            if(val-8<4 && lab[posX+1][posY].acceso==1){
-                calc_dist(posX+1, posY,dist+1);
-                fin = 0;
-            }
-            if(val-12<2 && lab[posX][posY-1].acceso==1){
-                calc_dist(posX, posY-1,dist+1);
-                fin = 0;
-            }
-            if(val-14<1 && lab[posX-1][posY].acceso==1){
-                calc_dist(posX-1, posY,dist+1);
-                fin = 0;
+            (*lab)[posX][posY].acceso=0;
+            if(posX != finX || posY != finY){
+                if(val<8){
+                    if((*lab)[posX][posY+1].acceso==1){
+                        distancia = calc_dist(posX, posY+1,dist+1,finX,finY,calc_dist,lab);
+                        fin = 0;
+                    }
+                }
+                else{
+                    val = val - 8;
+                }
+                if(val<4){
+                    if((*lab)[posX-1][posY].acceso==1){
+                        distancia = calc_dist(posX-1, posY,dist+1,finX,finY,calc_dist,lab);
+                        fin = 0;
+                    }
+                }
+                else{
+                    val = val - 4;
+                }
+                if(val<2){
+                    if((*lab)[posX][posY-1].acceso==1){
+                        distancia = calc_dist(posX, posY-1,dist+1,finX,finY,calc_dist,lab);
+                        fin = 0;
+                    }
+                }
+                else{
+                    val = val - 2;
+                }
+                if(val<1){
+                    if((*lab)[posX+1][posY].acceso==1){
+                        distancia = calc_dist(posX+1, posY,dist+1,finX,finY,calc_dist,lab);
+                        fin = 0;
+                    }
+                }
             }
             
-            if(fin==1){
-            }
-            else{
-            }
+            (*lab)[posX][posY].acceso=1;
             
-            return 0;
+            if(fin==1 && posX == finX && posY == finY){
+                if(dist < distancia || distancia == -1){
+                    return dist;
+                }
+                else{
+                    return distancia;
+                }
+            }
+            return distancia;
         };
-        return "none";
-    };
+        int dist1 = calc_dist(ys,xs,0,yr,xr,calc_dist,&lab);
+        distancia = -1;
+        int dist2 = calc_dist(yr,xr,0,ys,xs,calc_dist,&lab);
+        cout << dist1 << " " << dist2 << endl;
+    //};
     
-
+   // respuesta();
     // Write an action using cout. DON'T FORGET THE "<< endl"
     // To debug: cerr << "Debug messages..." << endl;
 
-    cout << respuesta() << endl;
+    //cout << respuesta() << endl;
 }
