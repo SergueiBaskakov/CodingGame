142d6f1490618f24924932b165c5a3d3e6da73ea
SergueiBaskakov <46057775+SergueiBaskakov@users.noreply.github.com>
2020-02-18 12:23:07 -0500

    Sin terminar

--- FactorialsOfPrimesDecomposition.cpp
+++ FactorialsOfPrimesDecomposition.cpp
@@ -14,7 +14,7 @@ using namespace std;
  **/
 bool primo(int numero){
     int t = int(sqrt(numero));
-    for(int i = 2; i < t; i++){
+    for(int i = 2; i <= t; i++){
         if(numero%i==0){
             return false;
         }
@@ -32,21 +32,40 @@ struct Numero{
     int n = 0;
 };
 
-list<Numero> listaPrimos(int numero){
-    int t = int(numero/2);
+list<Numero> listaPrimos(int numerador, int denominador){
+    int t = int(max(numerador,denominador)/2);
     list<Numero> l;
-    for (int i = 2; i < t; i++){
-        if(numero%i == 0){
+    for (int i = 2; i <= t; i++){
+        //if(numero%i == 0){
             if(primo(i)){
-                Numero temp1;
-                temp1.num = i;
-                temp1.n = temp1.n + 1;
-                l.push_back(temp1);
+                Numero temp;
+                temp.num = i;
+                if(numerador%i==0){
+                    int temporal = numerador;
+                    int a = 0;
+                    while(temporal%i == 0){
+                        a++;
+                        temporal = temporal/i;
+                    }
+                    temp.n = temp.n + a;
+                }
+                if(denominador%i==0 && denominador != 0){
+                    int temporal = denominador;
+                    int a = 0;
+                    while(temporal%i == 0){
+                        a++;
+                        temporal = temporal/i;
+                    }
+                    temp.n = temp.n - a;
+                }
+                l.push_back(temp);
             }
-        }
+        //}
     }
     return l;
 }
+
+
  
 int main(){
     string n;
@@ -67,10 +86,10 @@ int main(){
     int num = stoi(a);
     int den = 0;
     if(frac){ den = stoi(b);}
-    
-
     // Write an action using cout. DON'T FORGET THE "<< endl"
     // To debug: cerr << "Debug messages..." << endl;
+    list<Numero> lNum = listaPrimos(num, den);
 
-    cout << num << endl;
+
+    cout << lNum.front().n << endl;
 }
