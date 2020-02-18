326f1123147ed5f68de70c7505f5b4dd29c52b34
SergueiBaskakov <46057775+SergueiBaskakov@users.noreply.github.com>
2020-02-18 11:58:27 -0500

    Sin terminar

--- FactorialsOfPrimesDecomposition.cpp
+++ FactorialsOfPrimesDecomposition.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <list>
+#include <cmath>
 
 
 using namespace std;
@@ -10,9 +12,43 @@ using namespace std;
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  **/
+bool primo(int numero){
+    int t = int(sqrt(numero));
+    for(int i = 2; i < t; i++){
+        if(numero%i==0){
+            return false;
+        }
+    }
+    if(numero == 1){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
 
-int main()
-{
+struct Numero{
+    int num = 0;
+    int n = 0;
+};
+
+list<Numero> listaPrimos(int numero){
+    int t = int(numero/2);
+    list<Numero> l;
+    for (int i = 2; i < t; i++){
+        if(numero%i == 0){
+            if(primo(i)){
+                Numero temp1;
+                temp1.num = i;
+                temp1.n = temp1.n + 1;
+                l.push_back(temp1);
+            }
+        }
+    }
+    return l;
+}
+ 
+int main(){
     string n;
     string a = "";
     string b = "";
@@ -29,7 +65,8 @@ int main()
         }
     }
     int num = stoi(a);
-    if(frac){ int den = stoi(b);}
+    int den = 0;
+    if(frac){ den = stoi(b);}
     
 
     // Write an action using cout. DON'T FORGET THE "<< endl"
